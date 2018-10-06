2a8ecf2953efa1d306adf5aca88bcdebd9bef705
Kevin Lopez <klopez2013@fau.edu>
2018-10-06 17:55:41 -0400

    New File
    Needs to be rebuilt

---
+++ ChildAlert/ChildAlert/Pair.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ChildAlert
+{
+    [Activity(Label = "ChildAlert", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+
+
+        }
+    }
+}
+

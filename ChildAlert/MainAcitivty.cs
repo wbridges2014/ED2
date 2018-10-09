f637098cd16c498a7f2d3fd75029ba6c3e97d384
Malique Johnson <43919368+Exrm@users.noreply.github.com>
2018-10-08 23:24:14 -0400

    Add files via upload

---
+++ ChildAlert/MainAcitivty.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace ChildAlert
+{
+    [Activity(Label = "MainAcitivty")]
+    public class Location : Activity
+    {
+        TextView welcomeText;
+        Button pingButton;
+        Button locateButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+
+            // welcomeText = FindViewById<TextView>(Resource.Id.welcomeText);
+            pingButton = FindViewById<Button>(Resource.Id.pingButton);
+            locateButton = FindViewById<Button>(Resource.Id.locateButton);
+
+            pingButton.Click += OnPingButtonClick;
+            locateButton.Click += OnLocateButtonClick;
+        }
+
+        void OnPingButtonClick(object sender, EventArgs e)
+        {
+            welcomeText.Text = "*Beep*";
+        }
+
+        void OnLocateButtonClick(object sender, EventArgs e)
+        {
+            welcomeText.Text = "*GPS Location*";
+            StartActivity(typeof(Location));
+        }
+    }
+}

eb9fe5e8b90575069c9216184935f938128d7078
Kevin Lopez <klopez2013@fau.edu>
2018-10-10 09:46:08 -0400

    Adding Location
    Merging location services manually to working-bt

---
+++ ChildAlert/ChildAlert/Location.cs
@@ -0,0 +1,30 @@
+﻿using Android.OS;
+using Android.App;
+using Android.Content.PM;
+using Plugin.Permissions;
+
+namespace ChildAlert
+{
+    [Activity(Label = "MainActivity")]
+    public class Locate : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Location;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this, savedInstanceState);
+
+            //LoadApplication(new );
+        }
+    }
+}
